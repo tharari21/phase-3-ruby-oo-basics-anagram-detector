cdb2836c5dfb7866705a339c7aee89d8d1ffcb32
Tomer Harari <tharari21@gmail.com>
2022-08-10 16:11:21 -0400

    commit tree 734d2bd4c6fe8ebdead113b414a2c558bdac72a8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+	def initialize(word)
+		@word = word
+	end
+	def match(words)
+		words.filter do |word|
+			@word.chars.sort == word.chars.sort
+		end
+	end
+end
